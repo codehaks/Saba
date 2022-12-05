02904996e92ab4df329a41f8d59c7a2d6418a66c
Hakim Ghods <ghods.hakim@hotmail.com>
2022-12-05 23:31:33 +0330

    rate limiter

--- src/Saba.Web/Program.cs
+++ src/Saba.Web/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
 using Saba.Web.Data;
 
@@ -6,13 +8,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<HafezDbContext>(options =>
 {
     options.UseSqlite("Data source=hafezdata.sqlite");
-    options.LogTo(Console.WriteLine);
+    //options.LogTo(Console.WriteLine);
 });
 
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("slowdown", windowOptions => {
+        windowOptions.Window=TimeSpan.FromSeconds(1);
+        windowOptions.PermitLimit = 1;
+        windowOptions.QueueLimit = 1;
+    });
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseRateLimiter();
 
 if (app.Environment.IsDevelopment())
 {
@@ -20,8 +32,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/api/fal", (HafezDbContext db) =>
+app.MapGet("/api/fal", (HafezDbContext db,[FromServices]ILogger<Program> logger,HttpContext http) =>
 {
+    logger.LogInformation("Req-> {mm:s} | {connection}",DateTime.Now, http.Connection.Id);
     var count = db.Fals.Count();
     var row = new Random().Next(1, count - 1);
     var fal = db.Fals.OrderBy(b => b.Id).Skip(row).FirstOrDefault();
@@ -31,6 +44,7 @@ app.MapGet("/api/fal", (HafezDbContext db) =>
     }
 
     return Results.BadRequest();
-}).WithOpenApi(); 
+}).RequireRateLimiting("slowdown")
+.WithOpenApi(); 
 
 app.Run();
